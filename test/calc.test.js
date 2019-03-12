27762ad6d8b8b82fff9df08346a0b21e53fbb0c9
wangding <408542507@qq.com>
2019-03-12 14:24:26 +0800

    commit

---
+++ test/calc.test.js
@@ -0,0 +1,17 @@
+describe("面积计算功能测试",function(){
+  it('宽是 10，高是 20 的矩形，面积是200',function(){
+    var rect = rectangle();
+    expect(rect.area(10,20).to.be.equal(200));
+  });
+
+  it('宽是 10，高是 20 的矩形，面积是200',function(){
+    var rect = rectangle();
+    expect(rect.area('10','20').to.be.equal(200));
+  });
+
+  
+
+
+
+
+});
